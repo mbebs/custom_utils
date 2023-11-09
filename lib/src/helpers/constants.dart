ce87d5b5782f5a12d6b82d51beac9f12fbe43817
mbebwo <36182527+mbebs@users.noreply.github.com>
2023-11-08 20:09:21 -0600

    Update constants.dart

--- lib/src/helpers/constants.dart
+++ lib/src/helpers/constants.dart
@@ -39,8 +39,8 @@ void showOptionsBottomSheet({
       context: context,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(0.0),
-          topRight: Radius.circular(0.0),
+          topLeft: Radius.circular(15.0),
+          topRight: Radius.circular(15.0),
         ),
       ),
       isScrollControlled: true,
